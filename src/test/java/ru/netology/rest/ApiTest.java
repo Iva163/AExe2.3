16b0733712d779c984268d6598eeb632d9ea0055
Ivan Borisov <ivan_borisov_88@vk.com>
2023-04-03 11:53:36 +0400

    add test fail

---
+++ src/test/java/ru/netology/rest/ApiTest.java
@@ -0,0 +1,28 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class ApiTest {
+
+    @Test
+    void shouldReturnDemo() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Ivan") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("ivan"))
+        ;
+    }
+
+}
